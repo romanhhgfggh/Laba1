55e0587de39691f917ee43528c1bf24b5ca9c2d2
romantyshchuk <roman.tychuk@gmail.com>
2025-09-09 23:05:02 +0300

    Create laba1.py

---
+++ laba1.py
@@ -0,0 +1,34 @@
+number = 70
+metrs = 30
+file = b"What`s up bro?"
+place = 'Poliske'
+users = {"Roman": "Taras"}
+books = ["Game of Thrones" , "Murder in the Sought Expres"]
+coordinates = (1678, 98, -308)
+none = None
+othernumbers = {1, 2, 3, 4}
+itscool = True
+
+
+print(file)
+print(users)
+print(place)
+print(coordinates)
+print(books)
+print(itscool)
+print(none)
+print(metrs)
+print(number)
+print(othernumbers)
+
+
+print(type(file))
+print(type(othernumbers))
+print(type(none))
+print(type(users))
+print(type(coordinates))
+print(type(books))
+print(type(itscool))
+print(type(place))
+print(type(metrs))
+print(type(number))
